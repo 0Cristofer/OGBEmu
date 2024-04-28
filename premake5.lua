b7e9d816ccbfa344517c4e111e03502c4e4a1500
Cristofer Oswald <cristoferoswald@gmail.com>
2024-04-27 20:46:05 -0400

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,66 @@
+workspace "OGBEmu"
+	architecture "x64"
+	startproject "OGBEmu"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+
+project "OGBEmu"
+	location "OGBEmu"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	defines
+	{
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+	}
+
+	links 
+	{
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
